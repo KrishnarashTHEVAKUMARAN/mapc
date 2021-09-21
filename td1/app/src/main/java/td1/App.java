e21cdfec693731b047be11128930e4367dbed1b6
Krishnarash <thevakumarankrishnarash@gmail.com>
2021-09-21 09:47:31 +0200

    modify td1/App pour afficher le menu avec gradle

--- td1/app/src/main/java/td1/App.java
+++ td1/app/src/main/java/td1/App.java
@@ -3,12 +3,28 @@
  */
 package td1;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import td1.step5.api.burger.BurgerBuilder;
+import td1.step5.api.burger.BurgerMeat;
+import td1.step5.api.burger.BurgerSauce;
+import td1.step5.api.restauration.Food;
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+import java.util.Arrays;
+import java.util.List;
+
+import static td1.step5.api.burger.MenuSize.BIG;
+import static td1.step5.api.burger.MenuType.FISH_MENU;
+import static td1.step5.api.burger.MenuType.MEAT_MENU;
+
+public class App {
+    public static void main(String args[]) {
+        Food alice_dinner = BurgerBuilder.order_menu("Fishy", FISH_MENU).cook();
+        Food bob_dinner = BurgerBuilder.order_menu("Beefy", MEAT_MENU).with_onions().with_cheese().cook();
+        Food charles_dinner = BurgerBuilder.order_personal("Own Style", BurgerMeat.BEEF, BIG).with_onions()
+                .with_cheese().with_sauce(BurgerSauce.BARBECUE).with_sauce(BurgerSauce.BEARNAISE)
+                .with_sauce(BurgerSauce.BURGER).with_cheese().cook();
+        List<Food> dinners = Arrays.asList(alice_dinner, bob_dinner, charles_dinner);
+        for (Food dinner : dinners) {
+            System.out.println(dinner);
+        }
     }
 }
